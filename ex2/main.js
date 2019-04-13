72a77a92ef1f95b3b1a6b15725ffdfd7f9d3fde7
thir13en <santiago.lopez@exolever.com>
2019-04-13 20:43:55 +0200

    achieved to nest different li elements

--- ex2/main.js
+++ ex2/main.js
@@ -1,13 +1,47 @@
-$( document ).ready(() => {
-	$('button')
-		.animate({left: 300}, 2000)
-		.animate({top: 200}, 1000)
-		.fadeOut(222)
-		.delay(2000)
-		.queue(function(next) {
-			$(this)
-			.fadeIn(222)
-			.html('Adeu!');
-			next();
-		})
-});
+const addElement = () => {
+	const text = $('input[name="elToAdd"]').val();
+	const parentText = $('input[name="parent"]').val();
+	const ulOpen = '<ul>';
+	const ulClose = '</ul>';
+	const liOpen = '<li>';
+	const liClose = '</li>';
+
+	if (text && text.length) {
+		/** there is no content yet */
+		if ($('.list-container').html() === '') {
+			$('.list-container').html(ulOpen + liOpen + text + liClose + ulClose);
+		} else {		
+			if (parentText) {
+				const referencedEl = getReferencedEl($('.list-container li'), parentText);
+				if (referencedEl) {
+					if(referencedEl.children('ul').length) {
+						referencedEl.children('ul').append(liOpen + text + liClose);
+					} else {
+						referencedEl.append(ulOpen + liOpen + text + liClose + ulClose);
+					}
+				}
+			} else {
+				$('.list-container > ul').append(liOpen + text + liClose);
+			}
+		}
+	}
+	cleanInputs();
+}
+
+const cleanInputs = () => {
+	$('input[name="elToAdd"]').val('');
+	$('input[name="parent"]').val('');
+}
+
+const getReferencedEl = (elements, text) => {
+	let element;
+
+	elements.each(el => {
+		if (elements[el].innerText.split('\n')[0] === text) {
+			return element = el;
+		}
+	});
+
+	return elements[element] ? $(elements[element]) : undefined;
+}
+
